d15e7cddda7e95a8ec7633406bcaa1c8e648bec6
jmetais2017 <julien.metais@eleves.ec-nantes.fr>
2020-10-09 15:47:36 -0400

    Poisson blending - problème de couleurs

--- TP2/Inpainting/matlab/poissonBlending.m
+++ TP2/Inpainting/matlab/poissonBlending.m
@@ -9,9 +9,49 @@ function [ dst ] = poissonBlending( src, target, alpha )
     % la valeur du laplacien cible
     
     % TODO Question 2 :
+    
+    %On calcule le gradient de la cible
+    A=[0 -1 0 ; -1 4 -1 ; 0 -1 0];
+    lap = double(imfilter(target, A));
+    
+    h = size(src, 1)
+    w = size(src, 2)
+    
+    %Pour l'initialisation, on colle directement la cible dans l'image
+    %à compléter
     alpha = double(repmat(alpha,[1,1,3]));
-    alpha = alpha./max(alpha(:));
+    alpha = alpha./max(alpha(:));    
     dst = double(src) .* alpha + double(target) .* (1-alpha);
+
+    prev_dst = dst;
+    
+    N = 15;
+    
+    %On propage itérativement le laplacien par méthode de Jacobi
+    for n = 1:N
+        for i = 1:h
+            for j = 1:w
+                if alpha(i,j)==0.0
+                    dst(i, j,:) = 0.25 * lap(i, j,:);
+                    if j>1
+                        dst(i, j,:) = dst(i, j,:) + 0.25 * prev_dst(i, j-1,:);
+                    end
+                    if j<w
+                        dst(i, j,:) = dst(i, j,:) + 0.25 * prev_dst(i, j+1,:);
+                    end
+                    if i>1
+                        dst(i, j,:) = dst(i, j,:) + 0.25 * prev_dst(i-1, j,:);
+                    end
+                    if i<h
+                        dst(i, j,:) = dst(i, j,:) + 0.25 * prev_dst(i+1, j,:);
+                    end
+                end
+            end
+        end
+        prev_dst = dst;
+    end
+    
     dst = uint8(dst);
+    
 end
 
