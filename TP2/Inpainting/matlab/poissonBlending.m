32c594b4aec7b5252c1239e50a2814ea91d4d442
clementbernardd <clementbernardd@gmail.com>
2020-10-09 22:53:58 +0200

    Ajout du fichier retirer et version qui semble marcher

--- TP2/Inpainting/matlab/poissonBlending.m
+++ TP2/Inpainting/matlab/poissonBlending.m
@@ -11,45 +11,34 @@ function [ dst ] = poissonBlending( src, target, alpha )
     % TODO Question 2 :
     
     %On calcule le laplacien de la cible
-    A=[0 -1 0 ; -1 4 -1 ; 0 -1 0];
-    lap = double(imfilter(target, A));
-    
-    h = size(src, 1)
-    w = size(src, 2)
-    
+    A=[-1 -1 -1; -1 8 -1; -1 -1 -1];
+    lap = imfilter(double(target), A);
+        
     %Pour l'initialisation, on colle directement la cible dans l'image
     alpha = double(repmat(alpha,[1,1,3]));
     alpha = alpha./max(alpha(:));    
-    dst = double(src) .* alpha + double(target) .* (1-alpha);
-
-    prev_dst = dst;
     
-    N = 20; %Le nombre d'itérations
+    dst= double(src);
     
+    N = 5000; %Le nombre d'itérations
+    
+   [x, y, c] = size(dst); 
+   
+   img = zeros(x, y, c); 
     %On propage itérativement le laplacien par méthode de Jacobi
     for n = 1:N
-        for i = 1:h
-            for j = 1:w
-                if alpha(i,j)==0.0 %On ne traite que les pixels cilbés par le masque
-                    dst(i, j,:) = 0.25 * lap(i, j,:); %Valeur du laplacien cible
-                    if j>1
-                        dst(i, j,:) = dst(i, j,:) + 0.25 * prev_dst(i, j-1,:); %Moyenne des voisins
-                    end
-                    if j<w
-                        dst(i, j,:) = dst(i, j,:) + 0.25 * prev_dst(i, j+1,:);
-                    end
-                    if i>1
-                        dst(i, j,:) = dst(i, j,:) + 0.25 * prev_dst(i-1, j,:);
-                    end
-                    if i<h
-                        dst(i, j,:) = dst(i, j,:) + 0.25 * prev_dst(i+1, j,:);
-                    end
-                end
-            end
-        end
-        prev_dst = dst; %On stocke l'image à l'itération précédente pour poursuivre la récursion
-    end
-    
+        
+%          Chaque pixel est egal a la moyenne de ses voisins + laplacian
+        img =1/9 *  (imfilter(dst, [1,1,1;1,1,1;1,1,1]) + lap);
+       
+%          On modifile l'image en consequence 
+       dst = double(src) .* alpha + img .* (1-alpha);
+        
+    end 
+       
+%         prev_dst = dst; %On stocke l'image à l'itération précédente pour poursuivre la récursion
+
+   
     dst = uint8(dst);
     
 end
