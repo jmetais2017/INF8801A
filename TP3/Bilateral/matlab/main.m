e458a4c5674cdd314f5002a97d39074f562cc3a7
clementbernardd <clementbernardd@gmail.com>
2020-10-23 12:46:34 +0200

    Fist tests

--- TP3/Bilateral/matlab/main.m
+++ TP3/Bilateral/matlab/main.m
@@ -29,7 +29,50 @@ src = double(imread([srcFolder 'low' ext])) + double(imread([srcFolder 'mid' ext
 % normalisation
 src = src - min(src(:));
 src = src./max(src(:));
-figure; imshow(src); title('Réduction uniforme linéaire')
+figure; imshow(src); title('Réduction uniforme linéaire');
 
 % Filtrage avec filtres Gaussien et bilatéral (Question 2)
+
+% Filtre Gaussien 
+
+src = double(imread([srcFolder 'low' ext])) + double(imread([srcFolder 'mid' ext])) + double(imread([srcFolder 'high' ext]));
+% Image avec les basses frequences : passe-bas
+
+gaussian = imgaussfilt(src);
+% On soustrait le resultat du passe-bas pour obtenir un passe-haut
+src = src - gaussian;
+
+figure; imshow(src); title('Gaussian');
+
+
+% Filtre Bilateral 
+
+src = double(imread([srcFolder 'low' ext])) + double(imread([srcFolder 'mid' ext])) + double(imread([srcFolder 'high' ext]));
+
+sigmaSpatial = 10;
+edgeMin = min(src(:));
+edgeMax = max(src(:));
+
+% bilateral = bilateralFilter(rgb2gray(src), [], edgeMin, edgeMax, sigmaSpatial ,sigmaSpatial, sigmaSpatial );
+
+bilateral = imbilatfilt(src);
+
+src = src - bilateral;
+
+figure; imshow(src); title('Bilateral');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
