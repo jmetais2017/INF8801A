0c8ca060ee5fabc9c4fb952f99c1c3659924b9d2
jmetais2017 <julien.metais@eleves.ec-nantes.fr>
2020-10-30 11:39:01 -0400

    amélioration résultats partie 2

--- TP3/Bilateral/matlab/main.m
+++ TP3/Bilateral/matlab/main.m
@@ -1,65 +1,96 @@
-% Fonction principale du TP sur le filtre bilat�ral
+% Fonction principale du TP sur le filtre bilatéral
 
 clc
 clear all
 close all
 
 %% Denoising
-% Il s'agit d'une simple application du filtre bilat�ral.
+% Il s'agit d'une simple application du filtre bilatéral.
 
 noise = rgb2hsv(imread('../data/taj-rgb-noise.jpg'));
 figure;
-imshow(noise(:,:,3)); title('Image originale (bruit�e)');
+imshow(noise(:,:,3)); title('Image originale (bruitée)');
 
 
 % TODO Question 1 :
-filtered = medfilt2(noise(:,:,3), [ 5 5 ], 'symmetric');
+
+%Application du filtre bilatéral
+filtered = bilateralFilter(noise(:,:,3), [], 0, 1, 10, 0.2); %sigmaSpatial et sigmaRange déterminés expérimentalement
+
+result = noise;
+result(:,:,3) = filtered;
+result = hsv2rgb(result); %Reconversion en RGB
 figure;
-imshow(filtered); title('Image filtr�e');
+imshow(result); title('Image filtrée');
+
+
 
 %% Tone mapping
-% Il s'agit de compresser la plage d'intensit�es d'une image en pr�servant
-% les d�tails. Pour cela, on diminue les contrastes globaux en conservant
+% Il s'agit de compresser la plage d'intensités d'une image en préservant
+% les détails. Pour cela, on diminue les contrastes globaux en conservant
 % les contrastes locaux.
 
-% lecture de l'image hdr (� partir de 3 expositions diff�rentes)
+% Lecture de l'image hdr (à partir de 3 expositions différentes)
 srcFolder = '../data/hdr/memorial/'; ext = '.png';
 src = double(imread([srcFolder 'low' ext])) + double(imread([srcFolder 'mid' ext])) + double(imread([srcFolder 'high' ext]));
 
-% normalisation
-src = src - min(src(:));
-src = src./max(src(:));
-figure; imshow(src); title('Réduction uniforme linéaire');
+src_hsv = rgb2hsv(src); %Conversion dans l'espace HSV
+src_V = src_hsv(:,:,3); %Extraction du canal V
 
-% Filtrage avec filtres Gaussien et bilatéral (Question 2)
+
+% Réduction d'histogramme
+red_src = src - min(src(:));
+red_src = red_src./max(red_src(:));
+figure; imshow(red_src); title('Réduction uniforme linéaire');
+
 
 % Filtre Gaussien 
 
-src = double(imread([srcFolder 'low' ext])) + double(imread([srcFolder 'mid' ext])) + double(imread([srcFolder 'high' ext]));
-% Image avec les basses frequences : passe-bas
-
-gaussian = imgaussfilt(src);
-% On soustrait le resultat du passe-bas pour obtenir un passe-haut
-src = src - gaussian;
-
-figure; imshow(src); title('Gaussian');
-
-
-% Filtre Bilateral 
-
-src = double(imread([srcFolder 'low' ext])) + double(imread([srcFolder 'mid' ext])) + double(imread([srcFolder 'high' ext]));
-
-% Utilisation d'un patch pour observer le bruit de l'image 
-patch = imcrop(src,[50,50,50,50]);
-% Calcul de la variance du bruit 
-patchVar = std2(patch)^2;
-% Degree de variation du filtre doit etre superieur au bruit de l'image 
-degree = 2*patchVar;
-bilateral = imbilatfilt(src,degree,2);
+gauss_hsv = src_hsv; %On part de l'ipmage HSV
+gauss_V = src_V; %On filtre sur le canal V
+filtered_gauss = imgaussfilt(gauss_V, 12);
 
 % Filtre passe-haut 
-src = src - bilateral;
-figure; imshow(src); title('Bilateral');
+gauss_V = gauss_V - filtered_gauss; %Extraction des hautes fréquences de l'image en soustrayant la version floutée
+
+%Normalisation
+gauss_V = gauss_V - min(gauss_V);
+gauss_V = gauss_V./max(gauss_V);
+gauss_hsv(:,:,3) = gauss_V;
+gaussian = hsv2rgb(gauss_hsv); %Reconversion en RGB
+
+%Normalisation du résultat
+gaussian = gaussian - min(gaussian(:));
+gaussian = gaussian./max(gaussian(:));
+figure; imshow(gaussian); title('Passe-haut Gaussien');
+
+
+% Filtre Bilateral
+
+bil_hsv = src_hsv; %On part de l'ipmage HSV
+bil_V = src_V; %On filtre sur le canal V
+filtered_bil = bilateralFilter(bil_V, [], min(bil_hsv(:)), max(bil_hsv(:)), 256, 0.09*max(bil_hsv(:))); %On utilise un sigmaSpatial élevé (ici 50% de la largeur de l'image)
+
+% Filtre passe-haut 
+bil_V = bil_V - filtered_bil; %Extraction des hautes fréquences de l'image en soustrayant la version floutée
+
+%Normalisation
+bil_V = bil_V - min(bil_V);
+bil_V = bil_V./max(bil_V);
+bil_hsv(:,:,3) = bil_V;
+bilateral = hsv2rgb(bil_hsv); %Reconversion en RGB
+
+%Normalisation du résultat
+bilateral = bilateral - min(bilateral(:));
+bilateral = bilateral./max(bilateral(:));
+figure; imshow(bilateral); title('Passe-haut bilatéral');
+
+
+
+
+
+
+
 
 
 
