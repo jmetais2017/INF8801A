1d3620d2e82745f510ae8e20b0e433fe2f8aae6b
jmetais2017 <julien.metais@eleves.ec-nantes.fr>
2020-11-25 01:45:07 -0500

    questions 3 et 4

--- TP5/matlab/main.m
+++ TP5/matlab/main.m
@@ -27,47 +27,47 @@ seamViz = src;
 for y = 1:size(src,1), seamViz(y,seam(y),:) = [1,0,0]; end;
 figure; imshow(seamViz); title('Meilleure seam');
 % 
-% %% Redimensionnement avec le seam carving
-% 
-% % downscaling horizontal
-% dst = seamCarving( src, size(src,1), size(src,2)/1.5);
-% figure; imshow(dst); title('Suppression de seams');
-% 
-% % upscaling horizontal
-% dst = seamCarving( src, size(src,1), size(src,2)*1.5);
-% figure; imshow(dst); title('Ajout de seams');
-% 
-% %% Seam Carving dans le domaine du gradient
-% 
-% src = double(imread('../data/camera.png'))/255;
-% %src = imresize(src,0.5);
-% %figure; imshow(src); title('Image originale');
-% newSize = [size(src,1)/1.5, size(src)/1.5];
-% chans = size(src,3);
-% 
-% % % Seam Carving sur les couleurs
-% dst = seamCarving( src, newSize(1), newSize(2) );
-% figure; imshow(dst); title('Dans le domaine des couleurs');
-% 
-% % Seam Carving sur le gradient
-% srcX = imfilter(src,[-1,1]/2,'replicate'); % gradient en X
-% srcY = imfilter(src,[-1;1]/2,'replicate'); % gradient en Y
-% 
-% % On ajoute les gradients comme canaux supplémentaires à l'image
-% src = cat(3,src,srcX,srcY);
-% 
-% % On applique le seamCarving (il faut que votre code puisse gérer
-% % un nombre indéfini de canaux !)
-% dst = seamCarving( src, newSize(1), newSize(2) );
-% 
-% % On récupère les gradients redimensionnés
-% dstX = dst(:,:,chans+1:2*chans); % gradient en X
-% dstY = dst(:,:,2*chans+1:3*chans); % gradient en Y
-% 
-% % Calcul du laplacien
-% lap = -(imfilter(dstX,[-1,1]/2,'replicate') + ...
-%         imfilter(dstY,[-1;1]/2,'replicate'));
-% 
-% % Résolution du problème de poisson (Lap(X) = Y avec Y donné)
-% dst = poissonIntegration( lap, dst(:,:,1:chans) );
-% figure; imshow(dst); title('Dans le domaine du gradient');
+%% Redimensionnement avec le seam carving
+
+% downscaling horizontal
+dst = seamCarving( src, size(src,1), size(src,2)/1.5);
+figure; imshow(dst); title('Suppression de seams');
+
+% upscaling horizontal
+dst = seamCarving( src, size(src,1), size(src,2)*1.5);
+figure; imshow(dst); title('Ajout de seams');
+
+%% Seam Carving dans le domaine du gradient
+
+src = double(imread('../data/camera.png'))/255;
+%src = imresize(src,0.5);
+%figure; imshow(src); title('Image originale');
+newSize = [size(src,1)/1.5, size(src)/1.5];
+chans = size(src,3);
+
+% % Seam Carving sur les couleurs
+dst = seamCarving( src, newSize(1), newSize(2) );
+figure; imshow(dst); title('Dans le domaine des couleurs');
+
+% Seam Carving sur le gradient
+srcX = imfilter(src,[-1,1]/2,'replicate'); % gradient en X
+srcY = imfilter(src,[-1;1]/2,'replicate'); % gradient en Y
+
+% On ajoute les gradients comme canaux supplémentaires à l'image
+src = cat(3,src,srcX,srcY);
+
+% On applique le seamCarving (il faut que votre code puisse gérer
+% un nombre indéfini de canaux !)
+dst = seamCarving( src, newSize(1), newSize(2) );
+
+% On récupère les gradients redimensionnés
+dstX = dst(:,:,chans+1:2*chans); % gradient en X
+dstY = dst(:,:,2*chans+1:3*chans); % gradient en Y
+
+% Calcul du laplacien
+lap = -(imfilter(dstX,[-1,1]/2,'replicate') + ...
+        imfilter(dstY,[-1;1]/2,'replicate'));
+
+% Résolution du problème de poisson (Lap(X) = Y avec Y donné)
+dst = poissonIntegration( lap, dst(:,:,1:chans) );
+figure; imshow(dst); title('Dans le domaine du gradient');
--- TP5/matlab/seamCarving.m
+++ TP5/matlab/seamCarving.m
@@ -32,14 +32,47 @@ end
 function dst = shrinkH( src, newWidth )
 
     % TODO : Question 3
-    dst = imresize( src, [ size(src,1), newWidth ]);
+    height = size(src,1);
+    width = size(src,2);
+    nb_seams = width - newWidth; %Calcul du nombre de seams à retirer
     
+    dst = src;
+    
+    for k = 1:nb_seams
+        new_dst = ones(height, size(dst,2) - 1, size(dst,3)); %Création d'une nouvelle image de taille réduite
+        energy = getEnergy(dst); %Calcul de l'énergie de l'image courante
+        costs = pathsCost(energy);
+        seam = getSeam(costs); %Calcul du seam à retirer
+        for i = 1:height %Retrait du seam ligne par ligne
+            seam_id = seam(i);
+            new_dst(i,:,:) = cat(2, dst(i,1:(seam_id-1),:), dst(i,(seam_id+1):size(dst,2),:)); 
+        end
+        dst = new_dst; %Mise à jour de l'image courante
+    end 
 end
 
 % Duplique les pixels de seams verticales
 function dst = enlargeH( src, newWidth )
 
     % TODO : Question 4
-    dst = imresize( src, [ size(src,1), newWidth ]);
+    height = size(src,1);
+    width = size(src,2);
+    nb_seams = newWidth - width; %Calcul du nombre de seams à ajouter
     
+    dst = src;
+    energy = getEnergy(dst); %Calcul de l'énergie de l'image initiale
+    
+    for k = 1:nb_seams
+        new_dst = ones(height, size(dst,2) + 1, size(dst,3)); %Création d'une nouvelle image de taille augmentée
+        new_energy = ones(height, size(dst,2) + 1); %Carte d'énergie associée à la nouvelle image
+        costs = pathsCost(energy);
+        seam = getSeam(costs); %Calcul du seam à dupliquer
+        for i = 1:height
+            seam_id = seam(i);
+            new_dst(i,:,:) = cat(2, dst(i,1:(seam_id),:), dst(i,(seam_id):size(dst,2),:)); %Duplication du seam dans la nouvelle image
+            new_energy(i,:) = cat(2, energy(i,1:(seam_id - 1),:), 1.5*transpose([energy(i,seam_id); energy(i,seam_id)]), energy(i,(seam_id + 1):size(dst,2),:)); %Modification de la carte d'énergie
+        end
+        dst = new_dst; %Mise à jour de l'image courante
+        energy = new_energy;
+    end     
 end
